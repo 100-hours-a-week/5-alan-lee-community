cb346c6272a8de3da943da48a104c5a6dee2700e
bluegown <alan0308@naver.com>
2024-04-24 09:55:19 +0900

    js2

---
+++ js/server.js
@@ -0,0 +1,23 @@
+
+// express 모듈을 불러옵니다.
+const express = require("express");
+// 웹 서버가 사용할 포트 번호를 정의합니다.
+const port = 3000;
+const app = express();
+const path = require('path');
+/**
+* 루트 경로('/')에 대한 GET 요청을 처리
+* 요청이 오면 'Hello World!' 문자열을 응답
+*/
+// req = request(요청), res = response(응답)
+
+
+
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, 'login.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
